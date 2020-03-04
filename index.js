65310ec3b7eb8488add6d725d02faeceb7732a00
gajomon <vinicius.gajo@fullengenharia.com.br>
2020-03-04 13:16:12 -0300

    Implementando as funções que mostram os gráficos de velocidade e aceleração, todavia suas fórmulas devem ser checadas e é necessário implementar a parte de destruir os charts antes de plotar um novo gráfico

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 'use strict'
 
-var ctx = document.getElementById('myChart').getContext('2d');
+var ctxDisplacement = document.getElementById('myChartDisplacement').getContext('2d');
+var ctxVelocity = document.getElementById('myChartVelocity').getContext('2d');
+var ctxAcceleration = document.getElementById('myChartAcceleration').getContext('2d'); 
 
 const botao = document.getElementById('btnGerar');
 botao.addEventListener('click', plotGraphs);
+
+var chart1 = undefined,
+    chart2 = undefined,
+    chart3 = undefined;
 
 function plotGraphs(){
     let freq = document.getElementById('freq').value;
@@ -11,31 +17,79 @@ function plotGraphs(){
 
     let xLabel = genXLabel(freq);
     let yDisplacement = genYDisplacement(radFreq, xLabel);
+    let yVelocity = genYVelocity(radFreq, xLabel);
+    let yAcceleration = genYAcceleration(radFreq, xLabel);
+
+    if (typeof chart !== 'undefined') {
+        chart1.destroy()
+        .then(() => {
+            chart2.destroy();
+        })
+        .then(() => {
+            chart3.destroy();
+        })
+        .catch(err => console.log(err));
+        
+    }
 
     if ((xLabel.length > 0) && (yDisplacement.length > 0)){
-        var chart = new Chart(ctx, {
-            // The type of chart we want to create
+
+        chart1 = new Chart(ctxDisplacement, {
             type: 'line',
         
-            // The data for our dataset
             data: {
                 labels: xLabel,
                 datasets: [{
-                    label: 'My First dataset',
+                    label: `Deslocamento: frequência = ${freq}`,
                     backgroundColor: 'rgb(255, 99, 132)',
                     borderColor: 'rgb(255, 99, 132)',
                     data: yDisplacement
                 }]
             },
         
-            // Configuration options go here
             options: {}
         });
+
+        chart2 = new Chart(ctxVelocity, {
+            type: 'line',
+
+            data: {
+                labels: xLabel,
+                datasets: [{
+                    label: `Velocidade: frequência = ${freq}`,
+                    backgroundColor: 'rgb(255, 99, 132)',
+                    borderColor: 'rgb(255, 99, 132)',
+                    data: yVelocity
+                }]
+            },
+
+            options: {}
+        });
+
+        chart3 = new Chart(ctxAcceleration, {
+            type: 'line',
+        
+            data: {
+                labels: xLabel,
+                datasets: [{
+                    label: `Aceleração: frequência = ${freq}`,
+                    backgroundColor: 'rgb(255, 99, 132)',
+                    borderColor: 'rgb(255, 99, 132)',
+                    data: yAcceleration
+                }]
+            },
+        
+            options: {}
+        });
+
+
     }
 
     // Apaga os valores das variáveis
     xLabel = [];
     yDisplacement = [];
+    yVelocity = [];
+    yAcceleration = [];
 }
 
 // Taxa de amostragem = freq/10
@@ -43,8 +97,8 @@ function plotGraphs(){
 
 function genXLabel(freq) {
     let vec = [];
-    for (let cont = 0; cont < 10; cont += (freq/10)){
-        vec.push(cont);
+    for (let cont = 0; cont <= 10; cont += (1/(10*freq))){
+        vec.push(cont.toFixed(5));
     }
 
     return vec;
@@ -57,4 +111,22 @@ function genYDisplacement(radFreq, xLabel) {
     }
 
     return vecYD;
+}
+
+function genYVelocity(radFreq, xLabel) {
+    let vecYV = [];
+    for (let cont = 0; cont < xLabel.length; cont++) {
+        vecYV.push(radFreq * Math.cos(radFreq * xLabel[cont]));
+    }
+
+    return vecYV;
+}
+
+function genYAcceleration(radFreq, xLabel) {
+    let vecYA = [];
+    for (let cont = 0; cont < xLabel.length; cont++) {
+        vecYA.push((-radFreq) * radFreq * Math.sin(radFreq * xLabel[cont]));
+    }
+
+    return vecYA;
 }
